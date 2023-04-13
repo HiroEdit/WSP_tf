3deee6b0eb03c66c659e39279d2da6a686ae3ea8
Tyler <tfig88@gmail.com>
2023-04-12 20:34:02 -0400

    this still works?

--- main.py
+++ main.py
@@ -34,54 +34,54 @@ def get_url(url):
     length = len(products)
 
     def get_manufacturer():
-        for i in range(length):
-            brand = products[i].findAll("a")[1]
+        for x in range(length):
+            brand = products[x].findAll("a")[1]
             product_manufacturer.append(brand["data-brand"])
 
     def get_model():
-        for i in range(length):
-            model = products[i].findAll("a")[1]
+        for x in range(length):
+            model = products[x].findAll("a")[1]
             product_model.append(model["data-name"])
 
     def get_price():
-        for i in range(length):
-            price = products[i].findAll("a")[1]
+        for x in range(length):
+            price = products[x].findAll("a")[1]
             product_price.append(price["data-price"])
 
     def get_link():
-        for i in range(length):
-            link = products[i].findAll("a")[1]
+        for x in range(length):
+            link = products[x].findAll("a")[1]
             product_link.append("https://www.microcenter.com" + link["href"])
 
     def get_reviews():
-        for i in range(length):
-            review = products[i].span.text
+        for x in range(length):
+            review = products[x].span.text
             product_reviews.append(review.strip().replace("(", "").replace(")", ""))
 
     def get_rebate():
-        for i in range(length):
-            rebate = products[i].find("span", {"class": "price"}).text
+        for x in range(length):
+            rebate = products[x].find("span", {"class": "price"}).text
 
             if len(rebate) == 0:
-                product_rebate.append("0.00")
+                product_rebate.append(float("0.00"))
             else:
                 product_rebate.append(
                     rebate.replace("$", "").replace(",", ""))  # just want the value for later formatting
 
     def get_image():
-        for i in range(length):
-            find_image = products[i].find("img")
+        for x in range(length):
+            find_image = products[x].find("img")
             image = find_image.get("src")
             product_image.append(image)
 
     def get_sku():
-        for i in range(length):
-            sku = products[i].p.text
+        for x in range(length):
+            sku = products[x].p.text
             product_sku.append(sku)
 
     def get_offer():
-        for i in range(length):
-            offer = products[i].find("div", {"class": "highlight clear"}).text
+        for x in range(length):
+            offer = products[x].find("div", {"class": "highlight clear"}).text
 
             if len(offer) == 0:
                 product_offer.append("------")
@@ -90,33 +90,34 @@ def get_url(url):
 
     # where the product is purchasable
     def get_availability():
-        for i in range(length):
-            avail = products[i].find("p", {"class": "limit"})
+        for x in range(length):
+            avail = products[x].find("p", {"class": "limit"})
 
             if avail is not None:
-                avail = products[i].find("p", {"class": "limit"}).text  # In-store only
+                avail = products[x].find("p", {"class": "limit"}).text  # In-store only
                 product_availability.append(avail)
                 # print(avail)
 
             else:
-                unavail = products[i].find("p", {"class": "limitNoSale"})
+                unavail = products[x].find("p", {"class": "limitNoSale"})
 
                 if unavail is not None:
-                    unavail = products[i].find("p", {"class": "limitNoSale"}).text  # unavailable online
+                    unavail = products[x].find("p", {"class": "limitNoSale"}).text  # unavailable online
                     product_availability.append(unavail)
                     # print(unavail)
 
     def get_stars():
-        for i in range(length):
-            # products that have a star rating and don't have a rating both have this element so it's used as a starting point
-            stars = products[i].find("div", {"class": "ratingstars"})
+        for x in range(length):
+            # products that have a star rating and don't have a rating both have this element, so it's used as a
+            # starting point
+            stars = products[x].find("div", {"class": "ratingstars"})
             check_for_0 = stars.div.span.text  # should always be 0 reviews for products without rating
 
             if check_for_0 == "0 Reviews":
                 product_stars.append("No star rating")
 
             else:
-                find_stars = products[i].findAll("img")[2]
+                find_stars = products[x].findAll("img")[2]
                 stars = find_stars.get("alt")
                 product_stars.append(stars)
 
@@ -145,15 +146,14 @@ time.sleep(7)
 get_url(
     "https://www.microcenter.com/search/search_results.aspx?N=4294966937&NTK=all&NR=&sku_list=&page=4&myStore=false")
 
-# product prices and rebate prices are stored as strings in our lists. They need to be changed to float values to be able
-# to use num_format from xlsxwriter. You could just concatenate "$" to each item in the lists but when you open the file it'll have
-# a green arrows on all of the prices and rebate prices. Looks bad.
+# product prices and rebate prices are stored as strings in our lists. They need to be changed to float values to be
+# able to use num_format from xlsxwriter. You could just concatenate "$" to each item in the lists but when you open
+# the file it'll have a green arrows on all the prices and rebate prices. Looks bad.
 for i in range(len(product_price)):
     product_price[i] = float(product_price[i])
 
 for i in range(len(product_rebate)):
     product_rebate[i] = float(product_rebate[i])
-
 
 # searching our lists for the index with the greatest length to use that for column width formatting
 manufacturer_w = max(product_manufacturer, key=len)
@@ -163,7 +163,6 @@ link_w = max(product_link, key=len)
 image_w = max(product_image, key=len)
 offer_w = max(product_offer, key=len)
 availability_w = max(product_availability, key=len)
-
 
 ################################################## Start Excel ##################################################
 # https://xlsxwriter.readthedocs.io/tutorial02.html
@@ -211,7 +210,7 @@ for i in range(len(product_manufacturer)):
     worksheet1.write(i + 1, 5, product_stars[i])  # F
     worksheet1.write(i + 1, 6, product_sku[i])  # G
     worksheet1.write(i + 1, 7, product_offer[i])  # H
-    worksheet1.write(i + 1, 8, product_availability[i])  # I
+#    worksheet1.write(i + 1, 8, product_availability[i])  # I
     worksheet1.write(i + 1, 9, product_link[i])  # J
     worksheet1.write(i + 1, 10, product_image[i])  # K
 
